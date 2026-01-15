be3c77394d227d8a9252b8430406249f284151b8
Manabu Niseki <manabu.niseki@gmail.com>
2026-01-15 17:41:08 +0900

    feat: add Brand

---
+++ src/urlscan/pro/brand.py
@@ -0,0 +1,28 @@
+from urlscan.client import BaseClient
+
+
+class Brand(BaseClient):
+    def get_available_brands(self) -> dict:
+        """Get a list of brands that are tracked as part of urlscan's brand and phishing detection.
+
+        Returns:
+            dict: Response containing a list of brand objects.
+
+        Reference:
+            https://docs.urlscan.io/apis/urlscan-openapi/brands/availablebrands
+        """
+        return self.get_json("/api/v1/pro/availableBrands")
+
+    def get_brands(self) -> dict:
+        """Get a list of brands that we are able to detect phishing pages.
+
+        The total number of detected pages and the latest hit for each brand.
+        This is slower than the get_available method.
+
+        Returns:
+            dict: Response containing a list of brand object with detection statistics.
+
+        Reference:
+            https://docs.urlscan.io/apis/urlscan-openapi/brands/brandsummary
+        """
+        return self.get_json("/api/v1/pro/brands")

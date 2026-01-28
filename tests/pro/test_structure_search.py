4f4806f70f8731e7884db2617fdfbf4014b37b3c
Manabu Niseki <manabu.niseki@gmail.com>
2026-01-28 18:33:18 +0900

    test: add test with limit

--- tests/pro/test_structure_search.py
+++ tests/pro/test_structure_search.py
@@ -69,3 +69,24 @@ def test_structure_search_with_multiple_iterations(pro: Pro, httpserver: HTTPSer
     got = list(pro.structure_search(scan_id, q=q, size=10000))
     assert len(got) == 10001
     assert len(httpserver.log) == 2
+
+
+def test_structure_search_with_limit(pro: Pro, httpserver: HTTPServer):
+    scan_id = "test-scan-id"
+    q = "foo"
+
+    httpserver.expect_request(
+        f"/api/v1/pro/result/{scan_id}/similar/",
+        method="GET",
+        query_string={"q": q, "size": "100"},
+    ).respond_with_json(
+        {
+            "results": [{"sort": [i, "dummy"]} for i in range(1, 100)],
+            "has_more": True,
+            "total": 10000,
+        }
+    )
+
+    got = list(pro.structure_search(scan_id, q=q, limit=10))
+    assert len(got) == 10
+    assert len(httpserver.log) == 1
